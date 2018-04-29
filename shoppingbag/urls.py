2f1d32ef52f03e1330519178e59f1589f2f6a745
Hich <hichlist@yandex.ru>
2018-04-29 21:40:27 +0300

    Добавлена ссылка на корзину в базовом шаблоне

--- shoppingbag/urls.py
+++ shoppingbag/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.shoppingbag, name='shoppingsbag'),
+    url(r'^$', views.shoppingbag, name='shoppingbag'),
 ]
