fb7c3c9f45f7bc5f2beb474725ecce431be1b8d8
Hich <hichlist@yandex.ru>
2018-05-11 15:17:14 +0300

    Добавлена модель корзины

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 from products.models import Products
 
 
-# class Cart(models.Model):
-#     product = models.ForeignKey(Products, )
+class Status(models.Model):
+    name = models.CharField(max_length=30, default='New')
+
+
+class Cart(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField(verbose_name='quantity')
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, default=1)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True, blank=True)
